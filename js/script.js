d7d52437e69f5cbc9ceadb12272d44e0543d3142
MayVarela16 <loly_varela@hotmail.com>
2023-11-12 13:11:18 +0100

    termindo

--- js/script.js
+++ js/script.js
@@ -1,2 +1,38 @@
 import peliculas from './peliculas.js'
 
+document.addEventListener('DOMContentLoaded', function () {
+    const baseImageUrl = 'https://image.tmdb.org/t/p/w500';
+  
+
+    const mostrarPeliculas = (generoId, containerId) => {
+      const generoContainer = document.getElementById(containerId);
+  
+      
+      const peliculasGenero = peliculas.filter(pelicula => pelicula.genre_ids.includes(generoId));
+  
+    
+      peliculasGenero.forEach(pelicula => {
+        const peliculaElement = document.createElement('div');
+        peliculaElement.classList.add('pelicula');
+
+  
+        const imagenElement = document.createElement('img');
+        
+        imagenElement.src = baseImageUrl + pelicula.poster_path;
+        imagenElement.alt = pelicula.title;
+  
+        const tituloElement = document.createElement('p');
+        tituloElement.textContent = pelicula.title;
+  
+        peliculaElement.appendChild(imagenElement);
+        peliculaElement.appendChild(tituloElement);
+  
+        generoContainer.appendChild(peliculaElement);
+      });
+    };
+  
+    
+    mostrarPeliculas(28, 'genero-28'); 
+    mostrarPeliculas(53, 'genero-53'); 
+    mostrarPeliculas(12, 'genero-12'); 
+  });
